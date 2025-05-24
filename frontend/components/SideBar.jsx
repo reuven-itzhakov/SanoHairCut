5d9e8fd505fdd590c59a39fe483b377d5890662f
Reuven Itzhakov <itzhakovreuven@gmail.com>
2025-05-24 18:02:57 +0300

    sidebar fix

--- frontend/components/SideBar.jsx
+++ frontend/components/SideBar.jsx
@@ -3,7 +3,7 @@ import Guest from './SideBar/Guest.jsx';
 
 function SideBar({isOpen, setIsOpen}) {
     return(
-        <div className={`fixed top-0 right-0 w-[300px] h-screen transition-transform -translate-x-full translate-x-[${isOpen?0:300}px]`}>
+        <div className={`fixed top-0 right-0 w-[300px] h-screen transition-transform -translate-x-full ${isOpen? 'translate-x-0' : 'translate-x-[300px]'}`}>
             <div className="flex flex-col h-screen bg-gray-200">
                 <div className="flow-root p-[5px]">
                     <h1 className="float-left text-xl font-bold p-4">Sidebar</h1>

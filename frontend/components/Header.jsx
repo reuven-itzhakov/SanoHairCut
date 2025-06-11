3228315ff7b4047242735e4bde4f8165030512a2
Reuven Itzhakov <itzhakovreuven@gmail.com>
2025-06-11 15:14:17 +0300

    feat: enhance Header component with navigation functionality

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,12 +1,19 @@
 import SideBar from "./SideBar.jsx"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
     return (
         <>
         <header className="select-none flex items-center justify-between p-4 bg-gray-900 text-white">
-            <h1 className="cursor-pointer text-4xl font-bold hover:opacity-60 duration-300">mano.</h1>
+            <h1
+                className="cursor-pointer text-4xl font-bold hover:opacity-60 duration-300"
+                onClick={() => navigate("/")}
+            >
+                mano.
+            </h1>
             <svg onClick={() => setIsOpen(true)} className="cursor-pointer hover:opacity-60 duration-300" width="40px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"> {/*https://www.svgrepo.com/svg/369033/user-circle*/}
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
